b237682eda0ee959773be1d3bbd1c8757fbb7a4b
Moses Oyet <trickmaxillar@gmail.com>
2022-03-09 11:05:12 +0300

    Modify

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""`Amaenity` class that inherits from `BaseModel`
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Inherits from `BaseModel to create an amenity
+    Attributes:
+        name (str): to hold name of amenity
+    """
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""`State` class that inherits from `BaseModel`
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Inherits from `BaseModel to create a state
+    Attributes:
+        name (str): to hold state name
+    """
+
+    name = ""
