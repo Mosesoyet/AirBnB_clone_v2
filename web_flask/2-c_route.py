91030001156f929885abd1173bb789a0e024ec53
Moses Oyet David <trickmaxillar@gmail.com>
2022-04-10 02:14:32 +0300

    Add code

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Start a Flask application which listen on 0.0.0.0 port 500
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbtn():
+    """returns 'Hello HBNB'"""
+    return 'Hello HBNB'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun():
+    """returns a string to the c/<text> route"""
+    new = text.replace('_', ' ')
+    return 'C %s' % new
+
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0')
