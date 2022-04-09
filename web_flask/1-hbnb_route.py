f09e4be75c49a5eaaec9a12fa5abdc71060e5a17
Moses Oyet David <trickmaxillar@gmail.com>
2022-04-09 03:39:34 +0300

    Modify

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
-"""
-A script that starts a Flask web application
-"""
+"""Starts a Flask web application"""
 
 from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strick_slashes=False)
-def hello_school():
-    """Returns a string"""
+@app.route('/', strict_slashes=False)
+def hello_holberton():
+    """Returns a string at the root route"""
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strick_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Returns a string to /hbnb route"""
+    """Returns a string at the /hbnb route"""
     return 'HBNB'
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
