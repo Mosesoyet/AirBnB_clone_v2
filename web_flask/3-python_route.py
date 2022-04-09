8eafc6520bf1e0f764a72fdca6f15ee818626bbc
Moses Oyet David <trickmaxillar@gmail.com>
2022-04-10 02:39:21 +0300

    Modify

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Start a Flask application"""
+"""Starts a Flask web application"""
 
 from flask import Flask
 app = Flask(__name__)
@@ -7,29 +7,32 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_holberton():
-    """returns a string to the route"""
-    return "Hello HBNB!"
+    """Returns a string at the root route"""
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returns a string to the /hbnb route"""
-    return "HBNB"
+    """Returns a string at the /hbnb route"""
+    return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def cisfun():
-    """returns a string to the /c/<text> route"""
-    new = text.replace("_", " ")
-    return "C %s" % new
+def cisfun(text):
+    """Returns a string at the /c/<text> route,
+    expands the <text> variable"""
+    new = text.replace('_', ' ')
+    return 'C %s' % new
 
 
-@app.route('/python/(<text>)', strict_slashes=False)
-def python_iscool():
-    """returns a string to the /python/(<text>) route"""
-    nw = text.replace("_", " ")
-    return "Python %s" % nw
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text):
+    """Returns a string at the /python route, with a default text
+    of 'is cool', or the expansion of <text>"""
+    new = text.replace('_', ' ')
+    return 'Python %s' % new
 
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
