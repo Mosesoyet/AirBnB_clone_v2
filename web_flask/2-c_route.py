98d9088041a4407045140776e4ae53859e53073a
Moses Oyet David <trickmaxillar@gmail.com>
2022-04-10 02:25:03 +0300

    Modify

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Start a Flask application which listen on 0.0.0.0 port 500"""
+"""Starts a Flask web application"""
 
 from flask import Flask
 app = Flask(__name__)
@@ -7,22 +7,23 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_holberton():
-    """returns string to the route'"""
+    """Returns a string at the root route"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returns HBNB to the /hbnb route"""
+    """Returns a string at the /hbnb route"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def cisfun():
-    """returns a string to the c/<text> route"""
+def cisfun(text):
+    """Returns a string at the /c/<text> route,
+    expands the <text> variable"""
     new = text.replace('_', ' ')
     return 'C %s' % new
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(host='0.0.0.0')
